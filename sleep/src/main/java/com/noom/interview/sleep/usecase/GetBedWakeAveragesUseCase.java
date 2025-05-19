445eed93244526764af1e94e98a7b9b3d568797a
lucasthedev <lucas_wuthang@hotmail.com>
2025-05-19 09:37:04 -0300

    fixing bugs

--- sleep/src/main/java/com/noom/interview/sleep/usecase/GetBedWakeAveragesUseCase.java
+++ sleep/src/main/java/com/noom/interview/sleep/usecase/GetBedWakeAveragesUseCase.java
@@ -3,10 +3,13 @@ package com.noom.interview.sleep.usecase;
 import com.noom.interview.sleep.repository.SleepBedWakeAveragesRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 @Service
 public class GetBedWakeAveragesUseCase {
@@ -25,16 +28,27 @@ public class GetBedWakeAveragesUseCase {
         }
 
         double avgBedMinutes = bedTimes.stream()
-                .mapToInt(dt -> {
-                    int minutes = dt.getHour() * 60 + dt.getMinute();
-                    return (minutes < 18 * 60) ? minutes + 24 * 60 : minutes;
-                })
-                .average().orElse(0);
-        avgBedMinutes = avgBedMinutes % (24 * 60);
-
-        double avgWakeMinutes = wakeTimes.stream()
+                .filter(Objects::nonNull)
                 .mapToInt(dt -> dt.getHour() * 60 + dt.getMinute())
                 .average().orElse(0);
+
+        double avgWakeMinutes = wakeTimes.stream()
+                .filter(Objects::nonNull)
+                .mapToInt(dt -> dt.getHour() * 60 + dt.getMinute())
+                .average().orElse(0);
+
+        double avgBedDuration = IntStream.range(0, Math.min(bedTimes.size(), wakeTimes.size()))
+                .mapToLong(i -> {
+                    LocalDateTime bed = bedTimes.get(i);
+                    LocalDateTime wake = wakeTimes.get(i);
+                    if (bed != null && wake != null) {
+                        return Duration.between(bed, wake).toMinutes();
+                    }
+                    return 0;
+                })
+                .filter(min -> min > 0)
+                .average()
+                .orElse(0);
 
         LocalTime avgBedTime = LocalTime.of((int) (avgBedMinutes / 60), (int) (avgBedMinutes % 60));
         LocalTime avgWakeTime = LocalTime.of((int) (avgWakeMinutes / 60) % 24, (int) (avgWakeMinutes % 60));
